3abe198e21dd53a636928f843ed0faa5342a17b8
Rinat Khabibullin <rnt.hab.32@gmail.com>
2026-06-21 01:46:51 +0300

    Update landing page copy

--- src/data/site.ts
+++ src/data/site.ts
@@ -5,4 +5,4 @@ export const avitoUrl =
   "https://www.avito.ru/tuapse/predlozheniya_uslug/uslugi_elektrikamontazh_2629029815?context=H4sIAAAAAAAA_wE_AMD_YToyOntzOjEzOiJsb2NhbFByaW9yaXR5IjtiOjA7czoxOiJ4IjtzOjE2OiJlYTBOam1IMTVGU0xETXR3Ijt9vEkmTz8AAAA";
 
 export const brandName = "Электрика Туапсе";
-export const brandCaption = "команда электриков · 10+ лет";
+export const brandCaption = "команда электриков · 10+ лет опыта";
